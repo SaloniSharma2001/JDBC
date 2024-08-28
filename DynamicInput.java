1592e32294092d62d3b704ac53a46368f069a2d9
Saloni Sharma <saloni2019sharma@gmail.com>
2024-08-28 12:48:05 +0530

    Update DynamicInput.java

--- DynamicInput.java
+++ DynamicInput.java
@@ -12,7 +12,7 @@ class PreparedStatement {
         Connection con = DriverManger.getConnection(url, username, password);
         //Create a query
         String q = "insert into table1(tName,tCity) values (?,?)";
-        //get the prepared statement object
+        //Get the prepared statement object
         PreparedStatement pstmt = con.prepareStatement(q);
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter name :");
@@ -20,8 +20,8 @@ class PreparedStatement {
 		System.out.println("Enter city :");
 		String city = br.readLine();
         //set the values to the query
-        pstmt.setSting(1, name);
-        pstmt.setSting(2, city);
+        pstmt.setString(1, name);
+        pstmt.setString(2, city);
         //Since we are don't want any result set hence we shall use executeUpdate inseat of execute itself
         pstmt.executeUpdate();
         //We cannot give q as a parameter to the execute statement here as it will compile the q once again but in an incomplete way wihtout the value being set.
